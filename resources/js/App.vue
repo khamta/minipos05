5cd3dbaa5bbc30136afb10704c46b5be7c049c0b
khamta <khamta87@gmail.com>
2022-11-30 22:27:17 +0700

    first commit

---
+++ resources/js/App.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <h1>Hello Laravel + Vuejs</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Minipos5App",
+
+    data() {
+        return {};
+    },
+
+    mounted() {},
+
+    methods: {},
+    computed() {},
+    whatch: {},
+};
+</script>
+
+<style lang="scss" scoped></style>
